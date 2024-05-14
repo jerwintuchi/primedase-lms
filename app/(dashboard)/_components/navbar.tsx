f2a4d5d201cef3cbe50342ad7083bd8f61fbf0fb
Jerwin Peralta <Jerwin.Peralta@GTPH-LPC093.cscc.local>
2024-05-15 04:00:02 +0800

    ui: deleted unused logo

--- app/(dashboard)/_components/navbar.tsx
+++ app/(dashboard)/_components/navbar.tsx
@@ -2,7 +2,7 @@ import NavbarRoutes from "@/components/navbar-routes"
 import { MobileSidebar } from "./mobile-sidebar"
 
 export const Navbar = () => {
-    return (
+    return ( //NAVBAR WITH THE AVATARBOX
         <div className="p-4 border-b h-full flex items-center bg-yellow-300 shadow-sm">
             <MobileSidebar/>
             <NavbarRoutes/>
