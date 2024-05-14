0b54c46b2d3cbabdf5b0dd1eb90431d0a857f74f
Jerwin Peralta <Jerwin.Peralta@GTPH-LPC093.cscc.local>
2024-05-14 23:28:03 +0800

    ui: changed the text of avatarbox to purple

--- app/(dashboard)/_components/logo.tsx
+++ app/(dashboard)/_components/logo.tsx
@@ -3,8 +3,9 @@ import Image from "next/image";
 export const Logo = () => {
     return (
         <Image
-        height={130}
+        height={"130"}
         width={130}
+        priority={false}
         alt="logo"
         src="/primedase.svg"/>
     )
