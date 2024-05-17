b6c692e18aa08acdfbd89bd7fad9f00dd3b2034a
Jerwin Peralta <Jerwin.Peralta@GTPH-LPC093.cscc.local>
2024-05-18 05:06:26 +0800

    ui: tweaked the position of teacher sign-in card

--- app/(auth)/(routes)/teacher-sign-up/[[...teacher-sign-up]]/page.tsx
+++ app/(auth)/(routes)/teacher-sign-up/[[...teacher-sign-up]]/page.tsx
@@ -2,13 +2,16 @@ import { SignUp } from "@clerk/nextjs";
 
 export default function Page() {
   return (
-    <div>
-      <h1 className="text-3xl pb-4 text-purple-700 no-scroll">
+    <div className="flex flex-col items-center">
+      <h1 className="text-3xl pb-3.5 text-purple-700 ">
         <strong>
           Start your teaching journey now!
         </strong>
       </h1>
-      <SignUp path="/teacher-sign-up" />
+      <div className="pb-5">
+        <SignUp path="/teacher-sign-up"/>
+      </div>
+      
     </div>
   
 );
