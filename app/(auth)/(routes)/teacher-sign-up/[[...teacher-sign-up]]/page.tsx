d245551aa976599ee9913bf3f0b59bf8b1d1256b
Jerwin Peralta <jerwin_tuchi21@yahoo.com>
2024-05-19 06:56:48 +0800

    installed clerk-sdk-node

--- app/(auth)/(routes)/teacher-sign-up/[[...teacher-sign-up]]/page.tsx
+++ app/(auth)/(routes)/teacher-sign-up/[[...teacher-sign-up]]/page.tsx
@@ -1,7 +1,24 @@
+import { createUserWithRole } from "@/app/(auth)/mutator";
 import { SignUp } from "@clerk/nextjs";
+import router from "next/router";
+import { NextApiRequest } from "next/types";
+import toast from "react-hot-toast";
 
 export default async function Page() {
-
+  const handleSignUp = async (userData: any) => {
+    try {
+      await createUserWithRole(
+        { url: router.asPath } as NextApiRequest,
+        userData,
+        router.asPath.startsWith("/teacher-sign-up/") ? "teacher" : "student"
+      );
+      router.push("/"); // Redirect to a success page or perform any other desired action
+    } catch (error) {
+      toast.error("Error creating user:");
+      // Handle any errors during user creation (e.g., display error message)
+    }
+  };
+  
   return (
     <div className="flex flex-col items-center">
       <h1 className="text-3xl pb-3.5 text-purple-700 ">
--- app/(auth)/mutator.tsx
+++ app/(auth)/mutator.tsx
@@ -1,23 +1,30 @@
 import { clerkClient } from "@clerk/nextjs/server";
 import { NextApiRequest } from "next";
+import router from "next/router";
 import toast from "react-hot-toast";
 
-export async function createUserWithRole(req: NextApiRequest, userData: any, role: String) {
+export async function createUserWithRole(
+    req: NextApiRequest, 
+    userData: any,
+    role: "student" | "teacher") {
+
+
     if (!req || !req.url) {
       throw new Error('Invalid request');
     }
   
-    const route = req.url.startsWith("/teacher-sign-up/") ? 
-    "teacher" : "student";
+    const role = req.url.startsWith("/teacher-sign-up/") ? "teacher" :
+    req.url.startsWith("/sign-up/") ? "student" : null;
+
       try {
       const user = await clerkClient.users.createUser({
         ...userData, // Spread user data from your form or component
         privateMetadata: { 
-          role
+          role: `${role}`
          }, 
       });
       toast.success("Registered Successfully!", user);
-      // Handle successful user creation (e.g., redirect to dashboard)
+
     } catch (error) {
       toast.error("Error creating user:");
       // Handle any errors during user creation (e.g., display error message)
