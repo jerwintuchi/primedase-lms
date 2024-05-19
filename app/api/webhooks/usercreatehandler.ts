923b2d080c58b0c1b653652d3b53d858c322d3a0
Jerwin Peralta <jerwin_tuchi21@yahoo.com>
2024-05-19 22:59:44 +0800

    added an api handler for user creation

---
+++ app/api/webhooks/usercreatehandler.ts
@@ -0,0 +1,52 @@
+import { NextApiRequest, NextApiResponse } from 'next';
+import { PrismaClient } from '@prisma/client';
+import { Webhook, WebhookRequiredHeaders } from 'svix';
+import { headers } from 'next/headers';
+import { IncomingHttpHeaders } from 'http';
+import { NextResponse } from 'next/server';
+
+const prisma = new PrismaClient();
+
+export default async function handler(req: NextApiRequest, res: NextApiResponse) {
+  if (req.method !== 'POST') {
+    return res.status(405).json({ message: 'Method not allowed' });
+  }
+
+  const payload = req.body;
+  const headersList = headers();
+  const heads = {
+    "svix-id": headersList.get("svix-id"),
+    "svix-timestamp": headersList.get("svix-timestamp"),
+    "svix-signature": headersList.get("svix-signature"),
+  };
+
+  const wh = new Webhook(process.env.WEBHOOK_SECRET!);
+
+let evt: Event | null = null;
+
+
+try {
+    evt = wh.verify(
+    JSON.stringify(payload), 
+    heads as IncomingHttpHeaders & WebhookRequiredHeaders) as Event;
+ } catch (err) {
+    console.log((err as Error).message);
+    return NextResponse.json({}, {status: 400})
+ }   
+
+const eventType: EventType = evt.type;
+if (eventType === "user.created" || eventType === "user.updated") {
+    const {id, ...clerkAttributes} = evt.data;
+    console.log(id);
+    console.log(clerkAttributes);  // from the User model
+}
+
+type EventType = "user.created" | "user.updated" | "*";
+
+type Event = {
+    data: Record<string, string | number>,
+    object: "event",
+    type: EventType,
+    };
+  
+}
