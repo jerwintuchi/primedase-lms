7e71400c35e95da9007d99e41584603923e6e659
Jerwin Peralta <Jerwin.Peralta@GTPH-LPC093.cscc.local>
2024-05-21 04:25:41 +0800

    ftr: fixed the unauthorized access of teacher page if logged in without teacher role

--- components/navbar-routes.tsx
+++ components/navbar-routes.tsx
@@ -1,41 +1,43 @@
 "use client";
 
 import { UserButton } from "@clerk/nextjs";
-import { usePathname} from "next/navigation";
+import { usePathname } from "next/navigation";
 import { Button } from "./ui/button";
 import { LogOut } from "lucide-react";
 import Link from "next/link";
 import { auth } from "@clerk/nextjs/server";
 
 const NavbarRoutes = () => {
-    const pathname = usePathname();
-    
-    const isTeacherPage = pathname?.startsWith("/teacher");
-    const isPlayerPage = pathname?.includes("/chapter");
-    
-    
-    return(
-        <div className="flex gap-x-2 ml-auto">
-            {isTeacherPage || isPlayerPage ? (
-                <Link href="/">
-                <Button size ="sm" className="text-white bg-red-500 hover:bg-red-800 hover:text-white size-auto">
-                    <LogOut className="h-4 w-4 mr-2"/>
-                        Exit
-                </Button>
-                </Link>
-            ) : (
-                <Link href="/teacher/courses">
-                    <Button size="sm" variant="ghost" className="text-purple-900 bg-purple-400 
-                    hover:bg-purple-400/20 hover:text-purple-700 border-indigo-900 size-auto">
-                        Teacher Mode
-                    </Button>
-                </Link>
-            )}
-            <UserButton 
-            afterSignOutUrl="/"
-            />
-        </div>
-    )
-}
+  const pathname = usePathname();
 
-export default NavbarRoutes
+  const isTeacherPage = pathname?.startsWith("/teacher");
+  const isPlayerPage = pathname?.includes("/chapter");
+
+  return (
+    <div className="flex gap-x-2 ml-auto">
+      {isTeacherPage || isPlayerPage ? (
+        <Link href="/">
+          <Button
+            size="sm"
+            className="text-white bg-red-500 hover:bg-red-800 hover:text-white size-auto">
+            <LogOut className="h-4 w-4 mr-2" />
+            Back to Student Mode
+          </Button>
+        </Link>
+      ) : (
+        <Link href="/teacher/courses">
+          <Button
+            size="sm"
+            variant="ghost"
+            className="text-purple-900 bg-purple-400 
+                    hover:bg-purple-400/20 hover:text-purple-700 border-indigo-900 size-auto">
+            Teacher Mode
+          </Button>
+        </Link>
+      )}
+      <UserButton afterSignOutUrl="/" />
+    </div>
+  );
+};
+
+export default NavbarRoutes;
