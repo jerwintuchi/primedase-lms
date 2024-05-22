f495ef5c92a4cf3e53be553f6403a398b90a2289
Jerwin Peralta <Jerwin.Peralta@GTPH-LPC093.cscc.local>
2024-05-23 02:50:43 +0800

    working changes

--- app/api/webhooks/create-metadata/route.ts
+++ app/api/webhooks/create-metadata/route.ts
@@ -1,9 +1,9 @@
-import { PrismaClient } from '@prisma/client';
-import { Webhook, WebhookRequiredHeaders } from 'svix';
-import { headers } from 'next/headers';
-import { IncomingHttpHeaders } from 'http';
-import { NextResponse } from 'next/server';
-import { clerkClient } from '@clerk/nextjs/server';
+import { PrismaClient } from "@prisma/client";
+import { Webhook, WebhookRequiredHeaders } from "svix";
+import { headers } from "next/headers";
+import { IncomingHttpHeaders } from "http";
+import { NextResponse } from "next/server";
+import { clerkClient } from "@clerk/nextjs/server";
 
 const prisma = new PrismaClient();
 const webhookSecret = process.env.WEBHOOK_METADATA || "";
@@ -12,30 +12,31 @@ async function handler(request: Request) {
   const payload = await request.json();
   const headersList = headers();
   const heads = {
-    'svix-id': headersList.get('svix-id'),
-    'svix-timestamp': headersList.get('svix-timestamp'),
-    'svix-signature': headersList.get('svix-signature'),
+    "svix-id": headersList.get("svix-id"),
+    "svix-timestamp": headersList.get("svix-timestamp"),
+    "svix-signature": headersList.get("svix-signature"),
   };
 
   const wh = new Webhook(webhookSecret);
 
-  type EventType = 'user.created' | 'user.updated' | 'user.deleted';
+  type EventType = "user.created" | "user.updated" | "user.deleted";
 
   interface ClerkEvent {
     data: {
       id: string;
       publicMetadata?: Record<string, any>;
     };
-    object: 'event';
+    object: "event";
     type: EventType;
   }
 
   let evt: ClerkEvent | null = null;
 
-  
-
   try {
-    evt = wh.verify(JSON.stringify(payload), heads as IncomingHttpHeaders & WebhookRequiredHeaders) as ClerkEvent;
+    evt = wh.verify(
+      JSON.stringify(payload),
+      heads as IncomingHttpHeaders & WebhookRequiredHeaders
+    ) as ClerkEvent;
   } catch (err) {
     console.error((err as Error).message);
     return NextResponse.json({}, { status: 400 });
@@ -43,31 +44,30 @@ async function handler(request: Request) {
 
   const eventType = evt.type;
 
-  const { id,  publicMetadata } = evt.data;
-  const role = publicMetadata?.role || 'student'; // Default role to 'student'
+  const { id, publicMetadata } = evt.data;
+  const role = publicMetadata?.role || "student"; // Default role to 'student'
 
   //LOGIC FOR FIRST TIME USER CREATION
-  if(eventType === "user.created"){
+  if (eventType === "user.created") {
     const updateMetaData = await clerkClient.users.updateUserMetadata(id, {
-    publicMetadata: {
-      "role": role    //SET INITIALLY TO STUDENT SINCE DAPAT SA BACKEND LANG MANGYAYARI ANG ASSIGNMENT OF TEACHER/ADMIN ROLE
-    }
-  })
-  console.log(updateMetaData);
+      publicMetadata: {
+        role: role, //SET INITIALLY TO STUDENT SINCE DAPAT SA BACKEND LANG MANGYAYARI ANG ASSIGNMENT OF TEACHER/ADMIN ROLE
+      },
+    });
+    console.log(updateMetaData);
   }
-  
-  if (eventType === 'user.created' || eventType === 'user.updated') {
+
+  if (eventType === "user.created" || eventType === "user.updated") {
     // FOR CLERK DB
     await prisma.user.upsert({
       where: { clerkId: id },
       update: {
         clerkAttributes: {
-          publicMetadata: { 
-            "role": role
-          }
+          publicMetadata: {
+            role: role,
+          },
         },
-        
-      },  // FOR MY OWN POSTGRESQL DB
+      }, // FOR MY OWN POSTGRESQL DB
       create: {
         clerkId: id,
         role: role,
@@ -75,20 +75,25 @@ async function handler(request: Request) {
           ...publicMetadata,
           role: role,
         },
-        
       },
     });
 
-    return NextResponse.json({ message: 'User created or updated' }, { status: 200 });
-  } else if (eventType === 'user.deleted') {
+    return NextResponse.json(
+      { message: "User created or updated" },
+      { status: 200 }
+    );
+  } else if (eventType === "user.deleted") {
     await prisma.user.delete({
       where: { clerkId: evt.data.id },
     });
-    
-    return NextResponse.json({ message: 'User deleted' }, { status: 200 });
+
+    return NextResponse.json({ message: "User deleted" }, { status: 200 });
   }
 
-  return NextResponse.json({ message: 'Unhandled event type' }, { status: 400 });
+  return NextResponse.json(
+    { message: "Unhandled event type" },
+    { status: 400 }
+  );
 }
 
 export const GET = handler;
--- app/landing/about/page.tsx
+++ app/landing/about/page.tsx
@@ -1,7 +1,7 @@
-import React from 'react';
-import MainNav from '../mainnav';
-import Link from 'next/link';
-import { Button } from '@/components/ui/button';
+import React from "react";
+import MainNav from "../mainnav";
+import Link from "next/link";
+import { Button } from "@/components/ui/button";
 
 const AboutPage = () => {
   return (
@@ -9,61 +9,110 @@ const AboutPage = () => {
       <MainNav />
 
       <section className="hero flex flex-col justify-center items-center h-screen">
-      <img src="/book-open.svg" alt="Book Icon" className="book-image justify-center h-20 w-20 pb-5" />  {/* Position hero image in center */}
-        <div className="hero-content w-full mx-auto max-w-screen-lg">  {/* Center content horizontally */}
-          <h1 className="text-3xl font-bold text-purple-800 mb-4 text-center pb-10">About Our Learning Management System</h1>  {/* Center text horizontally */}
-          <p className="text-lg text-purple-700 mb-4 px-20 indent-16">  {/* Reduced margin and centered text */}
-          Empower educators, ignite student passion. Our Learning Management System (LMS) is your one-stop shop for creating engaging online learning experiences. Educators can save time managing courses, personalize learning paths, and gain valuable insights with progress tracking. Learners stay motivated with interactive elements, deepen understanding through discussion forums, and track their progress towards success. Ready to transform your online learning?  Contact us today!
+        <img
+          src="/book-open.svg"
+          alt="Book Icon"
+          className="book-image justify-center h-20 w-20 pb-5"
+        />{" "}
+        {/* Position hero image in center */}
+        <div className="hero-content w-full mx-auto max-w-screen-lg">
+          {" "}
+          {/* Center content horizontally */}
+          <h1 className="text-3xl font-bold text-purple-800 mb-4 text-center pb-10">
+            About Our Learning Management System
+          </h1>{" "}
+          {/* Center text horizontally */}
+          <p className="text-lg text-purple-700 mb-4 px-20 indent-16">
+            {" "}
+            {/* Reduced margin and centered text */}
+            Empower educators, ignite student passion. Our Learning Management
+            System (LMS) is your one-stop shop for creating engaging online
+            learning experiences. Educators can save time managing courses,
+            personalize learning paths, and gain valuable insights with progress
+            tracking. Learners stay motivated with interactive elements, deepen
+            understanding through discussion forums, and track their progress
+            towards success. Ready to transform your online learning? Contact us
+            today!
           </p>
-          <p className="text-lg text-purple-700 mb-4 px-20">  {/* Reduced margin and centered text */}
-            With features such as interactive quizzes, discussion forums, and progress analytics, our LMS aims to enhance the online learning experience for students of all levels.
+          <p className="text-lg text-purple-700 mb-4 px-20">
+            {" "}
+            {/* Reduced margin and centered text */}
+            With features such as interactive quizzes, discussion forums, and
+            progress analytics, our LMS aims to enhance the online learning
+            experience for students of all levels.
           </p>
         </div>
       </section>
 
       <section className="benefits pb-5">
-        <h2 className="text-2xl font-bold text-purple-800 mb-4 text-center justify-center pb-20">Benefits of Our LMS</h2>
-        <div className="benefits-grid grid grid-cols-1 md:grid-cols-2 gap-x-48 gap-y-10 px-20 ml-10 mb-10">  {/* Added grid-column-gap to benefits-grid class */}
+        <h2 className="text-2xl font-bold text-purple-800 mb-4 text-center justify-center pb-20">
+          Benefits of Our LMS
+        </h2>
+        <div className="benefits-grid grid grid-cols-1 md:grid-cols-2 gap-x-48 gap-y-10 px-20 ml-10 mb-10">
+          {" "}
+          {/* Added grid-column-gap to benefits-grid class */}
           <div className="benefit">
             <i className="fas fa-check-circle text-purple-700 text-3xl mb-2"></i>
-            <h3 className="text-lg font-medium text-purple-800 mb-2">Increased Engagement</h3>
-            <p className="text-base text-gray-600">Engaging features keep students motivated and improve learning outcomes. The skills and knowledge gained through engaging features prepare students for future academic and professional endeavors.
+            <h3 className="text-lg font-medium text-purple-800 mb-2">
+              Increased Engagement
+            </h3>
+            <p className="text-base text-gray-600">
+              Engaging features keep students motivated and improve learning
+              outcomes. The skills and knowledge gained through engaging
+              features prepare students for future academic and professional
+              endeavors.
             </p>
           </div>
           <div className="benefit">
             <i className="fas fa-chart-line text-purple-700 text-3xl mb-2"></i>
-            <h3 className="text-lg font-medium text-purple-800 mb-2">Improved Learning Outcomes</h3>
-            <p className="text-base text-gray-600">Track progress and personalize learning for better results. Detailed reports and analytics provide educators with valuable insights into student performance, allowing them to identify areas of strength and weakness.
+            <h3 className="text-lg font-medium text-purple-800 mb-2">
+              Improved Learning Outcomes
+            </h3>
+            <p className="text-base text-gray-600">
+              Track progress and personalize learning for better results.
+              Detailed reports and analytics provide educators with valuable
+              insights into student performance, allowing them to identify areas
+              of strength and weakness.
             </p>
           </div>
           <div className="benefit">
             <i className="fas fa-users text-purple-700 text-3xl mb-2"></i>
-            <h3 className="text-lg font-medium text-purple-800 mb-2">Streamlined Administration</h3>
-            <p className="text-base text-gray-600">Say goodbye to cumbersome course management and resource organization. The LMS empowers you to manage courses, users, and resources efficiently. With intuitive tools for course creation, user administration, and resource sharing, educators can focus on what matters most - delivering engaging and effective learning experiences. 
+            <h3 className="text-lg font-medium text-purple-800 mb-2">
+              Streamlined Administration
+            </h3>
+            <p className="text-base text-gray-600">
+              Say goodbye to cumbersome course management and resource
+              organization. The LMS empowers you to manage courses, users, and
+              resources efficiently. With intuitive tools for course creation,
+              user administration, and resource sharing, educators can focus on
+              what matters most - delivering engaging and effective learning
+              experiences.
             </p>
           </div>
         </div>
       </section>
 
       <p className="text-sm text-gray-700 text-center mb-4">
-        Contact us to learn more about how our LMS can benefit your educational institution or organization.
-        (+63) 91234567891
+        Contact us to learn more about how our LMS can benefit your educational
+        institution or organization. (+63) 91234567891
       </p>
 
       <section className="call-to-action">
         <div className="cta-container flex justify-center items-center px-20 py-8 bg-purple-700 text-white">
-          <h3 className="text-xl font-bold mb-2 mr-20">Ready to empower your learning experience?</h3>
+          <h3 className="text-xl font-bold mb-2 mr-20">
+            Ready to empower your learning experience?
+          </h3>
           <Link href="/sign-up">
-            <Button variant="ghost" className="bg-purple-500 hover:bg-purple-600 text-white font-bold py-2 px-4 rounded-md">
-                Start Your Free Trial
+            <Button
+              variant="ghost"
+              className="bg-purple-500 hover:bg-purple-600 text-white font-bold py-2 px-4 rounded-md">
+              Start Your Free Trial
             </Button>
           </Link>
-          
-          <div className='ml-10 flex items-center'>
-          <img src="/primedase-footer.svg" alt="Logo Icon"/>
-            <div className='text-xs mb-3.5'>
-                © 2024 Primedase.
-            </div>
+
+          <div className="ml-10 flex items-center">
+            <img src="/primedase-footer.svg" alt="Logo Icon" />
+            <div className="text-xs mb-3.5">© 2024 Primedase.</div>
           </div>
         </div>
       </section>
