215d053f3903ecc4446703dd530f72bbb9870c67
Jerwin Peralta <jerwin_tuchi21@yahoo.com>
2024-05-26 00:27:04 +0800

    ftr: fixed the get request loop on checkRole helper

--- app/(dashboard)/(routes)/page.tsx
+++ app/(dashboard)/(routes)/page.tsx
@@ -4,20 +4,18 @@ import { UserButton } from "@clerk/nextjs";
 import { auth, currentUser } from "@clerk/nextjs/server";
 import Link from "next/link";
 import { redirect } from "next/navigation";
-import { isTeacher } from "@/lib/teacher";
+import { isTeacher } from "@/lib/teacher"; // Likely not needed here
 
 export default async function TeacherDashBoard(role: Roles) {
   const user = await currentUser();
 
-  if (checkRole("student") && user) {
-    return redirect("/student");
-  } else if (
-    checkRole("teacher") || // checking for publicMetaData
-    (user?.privateMetadata?.role === "teacher" && //for privateMetaData
-      user)
-  ) {
+  if (checkRole("teacher") && user) {
+    // Redirect to root for teacher or publicMetadata.role === "teacher"
     return redirect("/");
+  } else if (checkRole("student") && user) {
+    return redirect("/student");
   }
+
   if (!user) {
     console.log("UNAUTHORIZED USER", checkRole("student"));
     console.log("UNAUTHORIZED USER", checkRole("teacher"));
--- app/utils/roles.ts
+++ app/utils/roles.ts
@@ -1,11 +1,11 @@
 import { auth } from "@clerk/nextjs/server";
 import { Roles } from "../types/globals";
-//doesn't support privateMetaData
+
 export const checkRole = (role: Roles) => {
   const { sessionClaims } = auth();
 
   console.log("checkRole called with role:", role);
-  //console.log("Session claims:", sessionClaims);
+  // console.log("Session claims:", sessionClaims);
 
-  return sessionClaims?.metadata.role === role ?? false;
+  return sessionClaims?.metadata?.role === role ?? false;
 };
