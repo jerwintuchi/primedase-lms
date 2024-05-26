e4682104ad78efae8c31b8a75407b81ca348cf84
Jerwin Peralta <jerwin_tuchi21@yahoo.com>
2024-05-27 01:36:21 +0800

    ui: fixed the border color from the sidebar-items

--- app/(dashboard)/_components/sidebar-item.tsx
+++ app/(dashboard)/_components/sidebar-item.tsx
@@ -1,12 +1,17 @@
 import { cn } from "@/lib/utils";
 import { usePathname, useRouter } from "next/navigation";
 import { SidebarItemProps } from "./sidebar-routes";
-export const SidebarItem: React.FC<SidebarItemProps> = ({ icon: Icon, label, href }) => {
+import "../../../app/globals.css";
+export const SidebarItem: React.FC<SidebarItemProps> = ({
+  icon: Icon,
+  label,
+  href,
+}) => {
   const pathname = usePathname();
   const router = useRouter();
 
   const isActive =
-    pathname === '/' && href === '/' || // If we are on the root page
+    (pathname === "/" && href === "/") || // If we are on the root page
     pathname === href || // For checking if we're on the exact same page
     pathname?.startsWith(`${href}/`); // For specific cases where we can be in a subroute of a specific route
 
@@ -19,18 +24,21 @@ export const SidebarItem: React.FC<SidebarItemProps> = ({ icon: Icon, label, hre
       onClick={onClick}
       type="button"
       className={cn(
-        'flex items-center gap-x-2 text-purple-900 text-sm font-semibold pl-6 transition-all hover:text-black-600 hover:bg-purple-300',
-        isActive && 'text-purple-900 bg-purple-200 hover:bg-purple-200 hover:text-purple-700 border-l-8' // Add transition class here
-      )}
-    >
+        "flex items-center gap-x-2 text-purple-900 text-sm font-semibold pl-6 transition-all hover:text-black-600 hover:bg-purple-300",
+        isActive &&
+          "text-purple-900 bg-purple-200 hover:bg-purple-200 hover:text-purple-700 border-l-8 border-purple-900" // Add transition class here
+      )}>
       <div className="flex items-center gap-x-2 py-4">
-        <Icon size={22} className={cn('text-purple-800', isActive && 'text-purple-800')} />
+        <Icon
+          size={22}
+          className={cn("text-purple-800", isActive && "text-purple-800")}
+        />
         {label}
       </div>
       <div
         className={cn(
-          'ml-auto h-full transition-all', // Remove opacity manipulation
-          isActive && 'border-purple-500 ' // Add border on active state
+          "ml-auto h-full transition-all", // Remove opacity manipulation
+          isActive && "border-purple-500 " // Add border on active state
         )}
       />
     </button>
