dabcb6e51abbca0255379af9cc3be8e51038a12b
Jerwin Peralta <jerwin_tuchi21@yahoo.com>
2024-05-26 09:35:36 +0800

    added seed for magic categories

---
+++ scripts/seed.ts
@@ -0,0 +1,28 @@
+const { PrismaClient } = require("@prisma/client");
+
+const database = new PrismaClient();
+
+async function main() {
+  try {
+    await database.categories.createMany({
+      data: [
+        { name: "Elemental Magic" },
+        { name: "Illusion Arts" },
+        { name: "Conjuration" },
+        { name: "Abjuration" },
+        { name: "Necromancy" },
+        { name: "Forbidden Art" },
+        { name: "Ancient Magic" },
+        { name: "Arcane Studies" },
+        { name: "ᛟᚾᚡᛖᚱ" },
+      ],
+    });
+
+    console.log("Categories of Magic seeded");
+  } catch (error) {
+    console.log("Error seeing the db categories", error);
+  } finally {
+    await database.$disconnect();
+  }
+}
+main();
